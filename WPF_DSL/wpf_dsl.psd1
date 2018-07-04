e6b9d2ec7b8d8b1b8a8a1363cfc2f44db2c1be7f
MikeShepard <mshepard70@gmail.com>
2018-07-04 13:36:48 -0500

    removed update-typedata from psm1

--- WPF_DSL/wpf_dsl.psd1
+++ WPF_DSL/wpf_dsl.psd1
@@ -12,7 +12,7 @@
 RootModule = '.\wpf_dsl.psm1'
 
 # Version number of this module.
-ModuleVersion = '0.9'
+ModuleVersion = '0.9.0'
 
 
 # ID used to uniquely identify this module
--- WPF_DSL/wpf_dsl.psm1
+++ WPF_DSL/wpf_dsl.psm1
@@ -28,8 +28,3 @@ Foreach($import in @($Public + $Private))
 
 Export-ModuleMember -Function $Public.Basename -alias 'Edit-Object'
 
-$p=new-object System.Windows.Controls.StackPanel
-if(-not ($p | get-member GEtControlByName)){
-  Update-TypeData -TypeName System.Windows.Controls.Panel -MemberType ScriptMethod -MemberName GetControlByName -Value $function:GetControlByName
-}
-Remove-Variable -Name p
