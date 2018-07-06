34d849ac88ad82535942fa080d727c7706aa6b12
Mike Shepard <mshepard70@gmail.com>
2018-07-05 20:32:04 -0500

    Fixed missing GetControlByName on panels and window (#35)

--- WPFBot3000/public/Grid.ps1
+++ WPFBot3000/public/Grid.ps1
@@ -66,6 +66,7 @@ function Grid {
         [Grid]::SetRow($_, $row)
         $objectCount += 1
     }
+    $Grid | add-member -MemberType ScriptMethod -Name GetControlByName -Value $function:GetControlByName
     $Grid | add-member -Name Window -MemberType ScriptProperty -Value {[System.Windows.Window]::GetWindow($this)}
     $Grid | add-member -Name GetControlValue -MemberType ScriptMethod -Value {$d = @{}
         $this.Children | ForEach-Object {if ($_| get-member GetControlValue) {
