bb7d0fa05ac133ef97334e6acd136f441c0d77e7
MikeShepard <mshepard@jackhenry.com>
2018-07-09 12:57:52 -0500

    ResolveURL example - Set-WindowOutput

--- Examples/ResolveShortURLs.ps1
+++ Examples/ResolveShortURLs.ps1
@@ -1,9 +1,13 @@
-Dialog {
+#illustrates the Set-WindowOutput function
+#based on https://jdhitsolutions.com/blog/powershell/6035/making-short-links-long-with-powershell-and-wpf/
+
+$w= Window {
    TextBox -name ShortUrl -property @{Width=300}
    Button Resolve -Action{$txt=$this.Window.GetControlByName('ShortUrl')
-                          $resolved=(Invoke-WebRequest -UseBasicParsing –Uri $($txt.Text)).baseresponse.ResponseUri.AbsoluteURI
+                          $resolved=(Invoke-WebRequest -UseBasicParsing -Uri $($txt.Text)).baseresponse.ResponseUri.AbsoluteURI
                           Set-WindowOutput -window $this.Window -OutputValue ([PSCustomObject]@{ShortURL=$txt.Text 
                                                                                                Resolved=$resolved}) -close
    }
 } -title 'Link Lookup' -labelMap @{ShortURL='Enter a short url'}
-
+$w.ShowDialog() | out-null
+$w.GetWindowOutput()
