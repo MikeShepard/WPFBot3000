eaf24fb56254dffd88c12a3fe1be3b6ab693fb08
Mike Shepard <mshepard70@gmail.com>
2018-07-12 10:04:38 -0500

    Added manual positioning to grid, closes #76 (#82)

---
+++ WPFBot3000/Examples/grid3x4_manual.ps1
@@ -0,0 +1,12 @@
+﻿# Simple example showing how to put items in a grid manually.  
+# Rows that don't have anything in them will automatically collapse (they are sized to their contents)
+
+Dialog {
+      Grid -columnCount 3 -rowCount 4 -Manual  {
+        Label Hello -property @{'Grid.Column'=2;'Grid.Row'=0}
+        Label Hello -property @{'Grid.Column'=0;'Grid.Row'=1}
+        Label Hello -property @{'Grid.Column'=1;'Grid.Row'=2}
+        Label Hello -property @{'Grid.Column'=2;'Grid.Row'=3}
+      } -property @{ShowGridLines=$true}
+   }
+
