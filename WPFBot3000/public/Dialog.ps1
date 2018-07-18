bc0fd4707caa04b7b5d1613aa7dfa75197e2a624
Mike Shepard <mshepard70@gmail.com>
2018-07-18 10:09:19 -0500

    Window show (#95)
    * Updated dialog to call window.ShowForValue
    
    * !deploy

--- WPFBot3000/public/Dialog.ps1
+++ WPFBot3000/public/Dialog.ps1
@@ -19,10 +19,10 @@ The window title
 
 .EXAMPLE
 Dialog {
-TextBox FirstName
-TextBox LastName
-TextBox EmailAddress
-DatePicker ReminderDate
+    TextBox FirstName
+    TextBox LastName
+    TextBox EmailAddress
+    DatePicker ReminderDate
 }
 
 # Displays a window with 3 textboxes and a date picker and if the user presses ok (instead of cancel) it outputs an object with 4 properties (populated from the controls)
@@ -50,8 +50,5 @@ function Dialog {
         } -Orientation Horizontal -Property @{}
     } @PSBoundParameters
     $w.Content.RowDefinitions[-1].Height = 'Auto'
-    $dialogResult = $w.Showdialog()
-    if ($dialogResult) {
-        $w.GetWindowOutput()
-    }
+    $w.ShowForValue()
 }
