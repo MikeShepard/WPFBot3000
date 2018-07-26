b38e106133019a328739f1fbf591ecb505233369
Mike Shepard <mshepard70@gmail.com>
2018-07-26 03:33:28 -0500

    111 top level aliases (#116)
    * Switched window and dialog to aliases, cleaned up build
    
    * Fixed tabcontrol, build defn
    
    * another build defn fix

--- build.ps1
+++ build.ps1
@@ -3,11 +3,13 @@ param ($Task = 'Default')
 # Grab nuget bits, install modules, set build variables, start build.
 Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
 
-if($env:Name){
+if($env:APPVEYOR_BUILD_FOLDER){
     Install-Module Psake, PSDeploy, BuildHelpers,Platyps -force
     Install-Module Pester -Force -SkipPublisherCheck
-    Import-Module Psake, BuildHelpers, Platyps
 }
+Import-Module Psake, BuildHelpers, Platyps
+
+
 Set-BuildEnvironment -Force
 
 Invoke-psake -buildFile .\psake.ps1 -taskList $Task -nologo
