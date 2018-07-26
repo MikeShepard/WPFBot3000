5af9b62f85216403b43836ac8070610de6c4f31b
Mike Shepard <mshepard70@gmail.com>
2018-07-25 20:19:08 -0500

    Allow listview display properties set with no content - closes #103

--- WPFBot3000/public/ListView.ps1
+++ WPFBot3000/public/ListView.ps1
@@ -32,34 +32,38 @@ General notes
 function ListView {
     [CmdletBinding()]
     Param([string]$name,
-          [Array]$contents = @(),
-          $initialValue,
-          [Hashtable]$property = @{},
-          [Array]$DisplayProperty)
+        [Array]$contents = @(),
+        $initialValue,
+        [Hashtable]$property = @{},
+        [Array]$DisplayProperty)
     $baseProperties = @{
         Name = $name
     }
-    $l=New-WPFControl -type System.Windows.Controls.ListView -Properties $baseProperties,$property
-    $g=New-WPFControl -type System.Windows.Controls.GridView -properties @{AllowsColumnReorder=$true}
-    $l.View=$g
-    if ($Contents) {
-        if(-not $displayProperty){
-            $displayProperty=$contents[0]| get-member -MemberType Properties | select-object -ExpandProperty Name
-        }
-        foreach ($prop in $displayProperty){
-            $column=New-WPFControl System.Windows.Controls.GridViewColumn -properties @{Header=$prop;DisplayMemberBinding=(new-object System.Windows.Data.Binding $prop)}
+    $l = New-WPFControl -type System.Windows.Controls.ListView -Properties $baseProperties, $property
+    $g = New-WPFControl -type System.Windows.Controls.GridView -properties @{AllowsColumnReorder = $true}
+    $l.View = $g
+    if ($DisplayProperty) {
+        foreach ($prop in $displayProperty) {
+            $column = New-WPFControl System.Windows.Controls.GridViewColumn -properties @{Header = $prop; DisplayMemberBinding = (new-object System.Windows.Data.Binding $prop)}
             $g.Columns.Add($column)
         }
-        $l.ItemsSource=$contents
+    }
+
+    if ($Contents) {
+        if (-not $displayProperty) {
+            $displayProperty = $contents[0]| get-member -MemberType Properties | select-object -ExpandProperty Name
+        }
+        $l.ItemsSource = $contents
         if ($initialValue -and $_ -eq $initialValue) {
-           $l.SelectedItem = $_
+            $l.SelectedItem = $_
         }
     }
     $l | add-member -Name Window -MemberType ScriptProperty -Value {[System.Windows.Window]::GetWindow($this)}
-    $l | add-member -MemberType ScriptMethod -Name GetControlValue -Value {$item=$this.SelectedItem
-                                                                           if($item -is [ListViewitem] -and $item.Tag){
-                                                                               $item.Tag
-                                                                           } else {
-                                                                               $item
-                                                                           }} -PassThru
+    $l | add-member -MemberType ScriptMethod -Name GetControlValue -Value {$item = $this.SelectedItem
+        if ($item -is [ListViewitem] -and $item.Tag) {
+            $item.Tag
+        }
+        else {
+            $item
+        }} -PassThru
 }
