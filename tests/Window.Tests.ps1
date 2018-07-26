45d0113ba0b1708b0b0522c28af97fa7566da755
Mike Shepard <mshepard70@gmail.com>
2018-07-25 20:53:37 -0500

    added wpf test of window output

---
+++ tests/Window.Tests.ps1
@@ -0,0 +1,10 @@
+Describe "Window functionality"{
+    It "Returns an object for a textbox" {
+        $w=Window { TextBox Name -initialValue Fred}
+        $w.GetWindowOutput().Name | should be Fred
+    }
+    #It "Returns a complex object for nested controls" {
+    #    $w=Window {Grid  {TextBox Name -InitialValue Fred} -name SP}
+    #    $w.GetWindowOutput().SP.Name | Should Be Fred
+    #}
+}
