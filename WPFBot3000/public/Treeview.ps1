e73b805e959312b9278e3b0a4c2fa2e071d54339
MikeShepard <mshepard70@gmail.com>
2018-09-19 11:31:04 -0500

    fix typo in tab, remove warning in tree

--- WPFBot3000/public/Treeview.ps1
+++ WPFBot3000/public/Treeview.ps1
@@ -34,9 +34,6 @@ function Treeview {
         Name = $Name
     }
     $tree = New-WPFControl -type System.Windows.Controls.TreeView -Properties $baseProperties, $property
-    if ($Contents) {
-        Write-Warning 'TreeView -Contents parameter is obsolete.  Use .AddTreeViewItem() method instead.'
-    }
     $tree | add-member -Name AddTreeViewItem -membertype ScriptMethod -value ${function:Add-TreeViewItem}
     $tree | add-member -Name GetTreeViewItemByLabel -membertype ScriptMethod -value ${function:Get-TreeViewItemByLabel}
     $tree | add-member -Name GetTreeViewItemByObject -membertype ScriptMethod -value ${function:Get-TreeViewItemByObject}
