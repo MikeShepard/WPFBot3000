cdb99164844363caf983c2abe519207628d5db43
Mike Shepard <mshepard70@gmail.com>
2019-04-22 18:57:12 -0500

    Added Activate() call to Load event to bring window to front

--- WPFBot3000/public/Window.ps1
+++ WPFBot3000/public/Window.ps1
@@ -61,6 +61,7 @@ function Window {
         Margin        = 10
     }
     $w = New-WPFControl -type system.windows.window -properties $BaseProperties, $Property 
+    $w.Add_Load({$w.Activate()}.GetNewClosure())
     [array]$windowContent = & $Contents
     if ($windowContent.Count -gt 1) {
         $windowContent = StackPanel {$windowContent} -Orientation Vertical
