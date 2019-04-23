2607a893e86ea49214f21b02066d1e5d946ede5d
Mike Shepard <mshepard70@gmail.com>
2019-04-22 20:47:17 -0500

    Added parameter help for Setter and Style

--- WPFBot3000/public/Style.ps1
+++ WPFBot3000/public/Style.ps1
@@ -3,6 +3,10 @@
     Compare to a Style object...contains Setter objects which say what properties to set to what values
 .DESCRIPTION
     Compare to a Style object...contains Setter objects which say what properties to set to what values
+.PARAMETER Setters
+    A collection of setter objects to add to the style
+.PARAMETER Type
+    Optionally, the type to apply the style to.  If not supplied, the style will be added to the type of the object that contains this style
 .EXAMPLE
     TextBlock -name Name -text Fred -property @{Resources=(style {setter ([System.Windows.Controls.TextBlock]::BackgroundProperty) ([Brushes]::Green)})}
     #use as a value of the Resources key in the property hashtable for a control or a window
