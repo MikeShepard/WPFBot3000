20179e7cd80653a6beeed62bf45665a42ef9e8da
Mike Shepard <mshepard70@gmail.com>
2019-04-22 20:47:17 -0500

    fixed typename parameter help in Style

--- WPFBot3000/public/Setter.ps1
+++ WPFBot3000/public/Setter.ps1
@@ -3,6 +3,10 @@
       Setter object, used inside a style
 .DESCRIPTION
       Setter object, used inside a style, contains a property reference (either from a specific control or from the window) and a value
+.PARAMETER PropertyReference
+      A reference to a property (or the namme of a DependencyProperty).
+.PARAMETER Value
+      The value to be associated with the PropertyReference if it is not set by some other means.
 .EXAMPLE
       (style {setter BackgroundProperty ([Brushes]::Red) }
       #this style/setter says that the background property should be red.
