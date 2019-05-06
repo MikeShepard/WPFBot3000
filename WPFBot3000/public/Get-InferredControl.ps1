3021fbfd62c299bc156639631911ced25dd644de
Mike Shepard <mshepard70@gmail.com>
2019-05-05 19:47:48 -0500

    Added help and tests for inferred controls

--- WPFBot3000/public/Get-InferredControl.ps1
+++ WPFBot3000/public/Get-InferredControl.ps1
@@ -1,6 +1,24 @@
+<#
+.SYNOPSIS
+    Create a control whose type is inferred by the input
+.DESCRIPTION
+    If the input is already a WPF object (DependencyObject), Get-InferredControl will output it unaltered.
+    If the input is a hashtable, the output will be a combobox control named after the first key, with the corresponding values as items.
+    If the input is a string ending in Date, the output will be a DatePicker
+    Otherwise, the output will be a textbox.
+.EXAMPLE
+    PS C:\> & {'hello';'world'} | Get-InferredControl
+    #    ouptuts two textboxes with names of hello and world
+.INPUTS
+    Get-InferredControl accepts objects on the pipeline.
+.OUTPUTS
+    Various
+#>
 function Get-InferredControl {
     [Cmdletbinding()]
-    Param([Parameter(ValueFromPipeline = $True)]$Item)
+    Param(
+        #The item to infer a control from
+        [Parameter(ValueFromPipeline = $True)]$Item)
     process {
         write-verbose "Inferring control for $item"
         if ($item -is [System.Windows.DependencyObject]) {
